613268c888860419011460ca68c0fe81d3700a76
Md. Bokhtiar Nadeem Shawon <shawon.iitju.48@gmail.com>
2025-01-16 05:19:09 +0600

    first commit, srs document added

---
+++ SkillSwapHub/src/test/java/com/skillswaphub/SkillSwapHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillswaphub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
